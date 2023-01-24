8f2ac3fd1ac4e88b018a1615b2f51c728d699a33
Mark <crowleymark25@gmail.com>
2023-01-24 14:05:55 +0000

    Adding the csv files from the website

---
+++ getListofLeaguePos.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+class Club:
+    def __init__(self, teamName):
+        self.teamName = teamName
+        self.positions = []
+        return
+
+
+
+class Season:
+    def __init__(self, year, league):
+        self.year = year
+        self.league = league
